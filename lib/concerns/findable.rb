f8f8787a5bda999858b4864128cbdc427ba6a53b
noisegaze <ben.stephanus@gmail.com>
2020-08-22 11:32:18 -0400

    Done.

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,14 @@
+module Concerns::Findable 
+
+   
+    
+    def self.find_by_name(name)
+        self.all.find do |object|
+            object.name == name
+        end
+      end
+    
+    
+    
+
+end
